335198975bfb715968c167dbcd0c8f3029a42c79
AEhere <47299969+AEhere@users.noreply.github.com>
2021-03-27 23:12:29 +0100

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,25 +1,38 @@
 // by AEhere
 // This code controls the IRIS-1 UAV
 // 
- 
 
 #include <Servo.h>
+//#include <AxisJoystick.h>
+//#include <DelegateJoystick.h>
+//#include <Joystick.h>
+//#include <XInverterJoystick.h>
+//#include <XYReplacerJoystick.h>
+//#include <YInverterJoystick.h>
 
 Servo ThrottleESCServo;  // create servo object to control the ESC
 
-int ThrottleESCPin = A2;
+#define THR_ESC_PIN 3
+#define THR_CONTROL_PIN A7
 
 int intThrottlePos = 0;    // variable to store the servo position
 
 
 void setup() {
   
-  ThrottleESCServo.attach(ThrottleESCPin);
-  
+  ThrottleESCServo.attach(THR_ESC_PIN);   //Attach the ESC channel
+//  pinMode(3, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  intThrottlePos = analogRead(THR_CONTROL_PIN);            // reads the value of the potentiometer (value between 0 and 1023)
+  intThrottlePos = map(intThrottlePos, 0, 1023, 30, 120);     // scale it to use it with the servo (value between 0 and 180)
+  ThrottleESCServo.write(intThrottlePos); 
+ 
+  delay(10);
 
+//  intThrottlePos = map(intThrottlePos, 0, 180, 0, 255);
+//  analogWrite(11,intThrottlePos);
+  
 }
