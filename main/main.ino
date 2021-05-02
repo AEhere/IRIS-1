5502c9faa13c91febcbf599f88d1a671c8a97c00
AEhere <47299969+AEhere@users.noreply.github.com>
2021-05-02 20:48:31 +0200

    Update main.ino
    defines for pinout

--- main/main.ino
+++ main/main.ino
@@ -3,47 +3,54 @@
 //
 
 #include <Servo.h>
+#include "RF24.h"
 
 Servo ThrottleESCServo;  // create servo object to control the ESC
 
-#define THR_ESC_PIN 3
-#define THR_CONTROL_PIN A0
+#define ESC_THR_PIN 3
+#define SRV_ELEV_PIN 4
+#define SRV_RUD_PIN 5
+#define SRV_AIL_R_PIN 9
+#define SRV_AIL_L_PIN 10
 
-#include "RF24.h"
+#define THR_CONTROL_PIN A0      //for local throttle control
 
-#define CE_PIN  7
+#define CE_PIN  7               //radio pins
 #define CSN_PIN 8
 
 // Start the radio object
 RF24 radio(CE_PIN, CSN_PIN);
 
-int intThrottlePos = 0;    // variable to store the servo position
-int intRXThrottlePos = 0;   
-int value;
+int intTHRPos = 0;     // variable to store the servo position
+int intRXThrottlePos = 0;   // received throttle value
+int intELEVPos = 0;
+int intRUDPos = 0;
+int intAILPos = 0;
 
 void setup() {
 
     Serial.begin(9600);
 
 
-  ThrottleESCServo.attach(THR_ESC_PIN);   //Attach the ESC channel
+  ThrottleESCServo.attach(ESC_THR_PIN);   //Attach the ESC channel
+
 //  pinMode(3, OUTPUT);
 
 }
 
 void loop() {
 
-  
-        intThrottlePos = analogRead(THR_CONTROL_PIN);                    // reads the value of the potentiometer (value between 0 and 1023)
-        ThrottleESCServo.write(map(intThrottlePos, 512, 0, 10, 170));   // scale it to use it with the servo (value between 0 and 180)
 
-        Serial.println(intThrottlePos);
+        intTHRPos = analogRead(THR_CONTROL_PIN);                    // reads the value of the potentiometer (value between 0 and 1023)
+        ThrottleESCServo.write(map(intTHRPos, 512, 0, 10, 170));   // scale it to use it with the servo (value between 0 and 180)
+
+        Serial.println(intTHRPos);
 
 
 
   delay(10);
 
-//  intThrottlePos = map(intThrottlePos, 0, 180, 0, 255);
-//  analogWrite(11,intThrottlePos);
+//  intTHRPos = map(intTHRPos, 0, 180, 0, 255);
+//  analogWrite(11,intTHRPos);
 
 }
